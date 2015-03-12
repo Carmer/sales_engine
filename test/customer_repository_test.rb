8440a374e511d6d949072d3a12854d3dbacd76b0
Carmer <carmerandrew@gmail.com>
2015-03-12 14:49:54 -0600

    Added SalesEngine and hooked up reopsitories to engine.

--- test/customer_repository_test.rb
+++ test/customer_repository_test.rb
@@ -8,10 +8,19 @@ class CustomerRepositortyTest < Minitest::Test
   end
 
   def test_it_creates_an_array_of_customer_objects
-    customers_data = Parser.new.parse("sample_customers.csv")
+    customers_data = Parser.new.parse("./data/","sample_customers.csv")
 
     customers_array = CustomerRepository.new(customers_data).customers
     assert_equal 19, customers_array.size
     assert_equal Array, customers_array.class
+  end
+
+  def test_it_can_pull_a_random_sample_customer
+    customers_data = Parser.new.parse("./data/", "sample_customers.csv")
+
+    customers = CustomerRepository.new(customers_data)
+    sample = []
+    10.times {sample << customers.random}
+    assert sample.uniq
   end
 end
