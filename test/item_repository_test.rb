cc4def599ba65cbea2e42cf0b63073bfb464ad50
androidgrl <kawaharajamie@gmail.com>
2015-03-12 17:26:04 -0600

    adding random test for item_repo

--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -1,16 +1,32 @@
 require "./test/test_helper"
 require "./lib/item_repository"
 
-class ItemRepositortyTest < Minitest::Test
+class ItemRepositoryTest < Minitest::Test
 
-  def test_it_exists
-    assert ItemRepository
+  def setup
     @item_data = Parser.new.parse("./data/", "sample_items.csv")
   end
 
-  def test_it_creates_an_array_of_item_objects
-    @item_data = ItemRepository.new(item_data).items
-    assert_equal 10, item_data.size
-    assert_equal Array, item_data.class
+  def test_it_exists
+    assert ItemRepository
   end
+
+  def test_it_creates_an_array_of_item_objects
+    item_data = ItemRepository.new(@item_data)
+    assert_equal 10, item_data.items.size
+    assert_equal Array, item_data.items.class
+  end
+
+  def test_it_has_an_all_method
+    item_data = ItemRepository.new(@item_data)
+    assert_equal 10, item_data.all.size
+    assert_equal Array, item_data.all.class
+  end
+
+  def test_it_has_a_random_method
+    item_data = ItemRepository.new(@item_data)
+    assert_equal Item, item_data.random.class
+  end
+
+
 end
