3bbf24347e06914b03647fe67cf459d845ea12ef
Carmer <carmerandrew@gmail.com>
2015-03-13 19:32:14 -0600

    Added find methods to invoice class.

--- lib/invoice_repository.rb
+++ lib/invoice_repository.rb
@@ -17,6 +17,39 @@ class InvoiceRepository
     @invoices.random
   end
 
-  def find_by_invoice_id
+  def find_by_id(id)
+    find_by_parameter(invoices, :id, id)
+  end
+
+  def find_invoice_by_customer_id(customer_id)
+    find_by_parameter(invoices, :cust_id, customer_id)
+  end
+
+  def find_all_invoices_by_customer_id(customer_id)
+    find_all_by_parameter(invoices, :cust_id, customer_id)
+  end
+
+  def find_invoice_by_merchant_id(merchant_id)
+    find_by_parameter(invoices, :cust_id, merchant_id)
+  end
+
+  def find_all_invoices_by_merchant_id(merchant_id)
+    find_all_by_parameter(invoices, :cust_id, merchant_id)
+  end
+
+  def find_invoice_by_created_at(created_at)
+    find_all_by_parameter(invoices, :created_at, created_at)
+  end
+
+  def find_all_invoice_by_created_at(created_at)
+    find_all_by_parameter(invoices, :created_at, created_at)
+  end
+
+  def find_invoice_by_updated_at(updated_at)
+    find_all_by_parameter(invoices, :updated_at, updated_at)
+  end
+
+  def find_all_invoice_by_updated_at(updated_at)
+    find_all_by_parameter(invoices, :updated_at, updated_at)
   end
 end
