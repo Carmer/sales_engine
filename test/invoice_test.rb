9ed9942010fe27ed8fe6f24303ec8646aa9ce8be
Carmer <carmerandrew@gmail.com>
2015-03-14 13:51:57 -0600

    Refactored invoice and merchant class.

--- test/invoice_test.rb
+++ test/invoice_test.rb
@@ -13,49 +13,49 @@ class InvoiceTest < Minitest::Test
 
   def test_it_can_instantiate_an_invoice_object
     data = @invoice[0]
-    inv = Invoice.new(data)
+    inv = Invoice.new(data, nil)
     assert inv
   end
 
   def test_it_has_an_invoice_id
     data = @invoice[0]
-    inv = Invoice.new(data)
+    inv = Invoice.new(data, nil)
     assert_equal "1", inv.id
   end
 
   def test_there_is_a_invoice_id_for_the_invoice
     data = @invoice[0]
-    inv = Invoice.new(data)
+    inv = Invoice.new(data, nil)
     assert_equal "1", inv.customer_id
   end
 
   def test_there_is_a_last_name_for_invoice
     data = @invoice[0]
-    inv = Invoice.new(data)
+    inv = Invoice.new(data, nil)
     assert_equal "26", inv.merchant_id
   end
 
   def test_there_is_a_status_for_an_invoice
     data = @invoice[0]
-    inv = Invoice.new(data)
+    inv = Invoice.new(data, nil)
     assert_equal "shipped", inv.status
   end
 
   def test_there_is_a_created_at_date
     data = @invoice[0]
-    inv = Invoice.new(data)
+    inv = Invoice.new(data, nil)
     assert_equal "2012-03-25 09:54:09 UTC", inv.created_at
   end
 
   def test_there_is_an_updated_date
     data = @invoice[0]
-    inv = Invoice.new(data)
+    inv = Invoice.new(data, nil)
     assert_equal "2012-03-25 09:54:09 UTC", inv.updated_at
   end
 
   def test_it_can_check_another_invoice_id
     data = @invoice[5]
-    inv = Invoice.new(data)
+    inv = Invoice.new(data, nil)
     assert_equal "6", inv.id
   end
 end
--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -18,28 +18,28 @@ class TestMerchant < Minitest::Test
   def test_a_merchant_has_a_name
     data = @merchants[0]
 
-    merchant = Merchant.new(data)
+    merchant = Merchant.new(data, nil)
     assert_equal "Schroeder-Jerde", merchant.name
   end
 
   def test_a_merchant_has_an_id
     data = @merchants[0]
 
-    merchant = Merchant.new(data)
+    merchant = Merchant.new(data, nil)
     assert_equal "1", merchant.id
   end
 
   def test_a_merchant_has_a_created_at
     data = @merchants[0]
 
-    merchant = Merchant.new(data)
+    merchant = Merchant.new(data, nil)
     assert_equal "2012-03-27 14:53:59 UTC", merchant.created_at
   end
 
   def test_a_merchant_has_an_updated_at
     data = @merchants[0]
 
-    merchant = Merchant.new(data)
+    merchant = Merchant.new(data, nil)
     assert_equal "2012-03-27 14:53:59 UTC", merchant.updated_at
   end
 
@@ -47,28 +47,28 @@ class TestMerchant < Minitest::Test
   def test_another_merchant_has_a_name
     data = @merchants[8]
 
-    merchant = Merchant.new(data)
+    merchant = Merchant.new(data, nil)
     assert_equal "Hand-Spencer", merchant.name
   end
 
   def test_another_merchant_has_an_id
     data = @merchants[8]
 
-    merchant = Merchant.new(data)
+    merchant = Merchant.new(data, nil)
     assert_equal "9", merchant.id
   end
 
   def test_another_merchant_has_a_created_at
     data = @merchants[8]
 
-    merchant = Merchant.new(data)
+    merchant = Merchant.new(data, nil)
     assert_equal "2012-03-27 14:53:59 UTC", merchant.created_at
   end
 
   def test_another_merchant_has_an_updated_at
     data = @merchants[8]
 
-    merchant = Merchant.new(data)
+    merchant = Merchant.new(data, nil)
     assert_equal "2012-03-27 14:53:59 UTC", merchant.updated_at
   end
 
