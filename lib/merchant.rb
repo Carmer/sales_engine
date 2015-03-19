2d61f6cbb42afbb7363224a4a875b3d333e2cad5
Carmer <carmerandrew@gmail.com>
2015-03-19 10:05:35 -0600

    Refactor.

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -38,58 +38,56 @@ class Merchant
 
   def favorite_customer
 
-    all_merchant_customers = find_successful_invoices.map do |invoice|
-      invoice.customer
-    end
+  @all_merchant_customers ||= find_successful_invoices.map do |invoice|
+    invoice.customer
+  end
 
-     all_merchant_customers.max_by do |customer|
-       all_merchant_customers.count(customer)
-     end
+   @all_merchant_customers.max_by do |customer|
+     @all_merchant_customers.count(customer)
    end
+ end
 
-  # private
+  def find_successful_invoices
+    @successful_invoices ||= repository.find_successful_invoices
 
-    def find_successful_invoices
-      @successful_invoices ||= repository.find_successful_invoices
+    @all_i ||= @successful_invoices.select do |invoice|
+      invoice.merchant_id == id
+    end
+  end
 
-      @all_i ||= @successful_invoices.select do |invoice|
-        invoice.merchant_id == id
+  def find_successful_invoice_items
+    @successful_invoice_items ||= repository.find_successful_invoice_items
+
+    @all_i_items ||= @successful_invoice_items.select do |i_item|
+      find_successful_invoices.any? do |invoice|
+        invoice.id == i_item.invoice_id
       end
     end
+  end
 
-    def find_successful_invoice_items
-      @successful_invoice_items ||= repository.find_successful_invoice_items
-
-      @all_i_items ||= @successful_invoice_items.select do |i_item|
-        find_successful_invoices.any? do |invoice|
-          invoice.id == i_item.invoice_id
-        end
-      end
+  def revenue_by_date(date)
+    @invoices_by_date ||= find_successful_invoices.find_all do |invoice|
+      Date.parse(invoice.created_at.to_s) == Date.parse(date.to_s)
     end
 
-    def revenue_by_date(date)
-      invoices_by_date = find_successful_invoices.find_all do |invoice|
-        Date.parse(invoice.created_at.to_s) == Date.parse(date.to_s)
-      end
-
-      invoice_items = invoices_by_date.flat_map do |invoice|
-        invoice.invoice_items
-      end
-
-      invoice_items.reduce(0)  do |sum , i_item|
-        sum + (i_item.quantity * i_item.unit_price) / 100.00
-      end
+    @invoice_items ||= @invoices_by_date.flat_map do |invoice|
+      invoice.invoice_items
     end
 
-    def total_merchant_revenue
-      find_successful_invoice_items.reduce(0) do |sum, i_item|
-        sum + (i_item.quantity * i_item.unit_price) / 100.00
-      end
+    @invoice_items.reduce(0)  do |sum , i_item|
+      sum + (i_item.quantity * i_item.unit_price) / 100.00
     end
+  end
 
-    def customers_with_pending_invoices
-      unsuccessful_invoices = invoices - find_successful_invoices
-
-      unsuccessful_invoices.map {|ui| ui.customer}
+  def total_merchant_revenue
+    find_successful_invoice_items.reduce(0) do |sum, i_item|
+      sum + (i_item.quantity * i_item.unit_price) / 100.00
     end
+  end
+
+  def customers_with_pending_invoices
+    unsuccessful_invoices = invoices - find_successful_invoices
+
+    unsuccessful_invoices.map {|ui| ui.customer}
+  end
 end
