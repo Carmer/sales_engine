f1d19ba68fc021531c714320832306fe4985c857
Carmer <carmerandrew@gmail.com>
2015-03-19 09:42:46 -0600

    Refactoring and decreased run-time with some memoization.

--- lib/finder.rb
+++ lib/finder.rb
@@ -1,20 +1,20 @@
 module Finder
 
-  def find_all_by_parameter(repository, column_header, input)
+  def find_all_by(repository, column_header, input)
     repository.select {|thing| thing.send(column_header) == input}
   end
 
-  def find_by_parameter(repository, column_header, input)
+  def find_by(repository, column_header, input)
     repository.find {|thing| thing.send(column_header) == input}
   end
 
-  def find_by_string_parameter(repository, column_header, input)
+  def find_as_string(repository, column_header, input)
     repository.find do |thing|
       thing.send(column_header).to_s.upcase == input.upcase
     end
   end
 
-  def find_all_by_string_parameter(repository, column_header, input)
+  def find_all_as_string(repository, column_header, input)
     repository.select do |thing|
       thing.send(column_header).to_s.upcase == input.upcase
     end
